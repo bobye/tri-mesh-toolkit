ebe3154cf55f21cae76d7ae3d90cd84787831dc3
bobye <yelpoo@gmail.com>
2011-10-19 13:22:29 +0800

    use eigs instead of svds

--- bin/matlab/TriMeshTKNystromSVD.m
+++ bin/matlab/TriMeshTKNystromSVD.m
@@ -2,9 +2,9 @@ function S = TriMeshTKNystromSVD( mat_A, mat_B )
 % return Nystrom approximated SVD
 
 [U, D, H] = svd(mat_A);
-ZU = [U*sqrt(D); mat_B*H*sqrt(inv(D))];
-ZH = [H*sqrt(D); mat_B*U*sqrt(inv(D))];
+ZU = [U*sqrt(D); mat_B*H*sqrt(pinv(D))];
+ZH = [H*sqrt(D); mat_B*U*sqrt(pinv(D))];
 
-S=svd(ZH'*ZU);
+S=abs(eigs(ZH'*ZU, 100));  
 end
 

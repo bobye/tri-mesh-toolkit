ceade787d4ba6bfe495ed627488d66a14f7fbcde
bobye <yelpoo@gmail.com>
2011-11-03 18:56:57 +0800

    2D accurate FMM

--- bin/matlab/MSS.m
+++ bin/matlab/MSS.m
@@ -1,8 +1,8 @@
-function ImageCode = MSS(fbase_filename, eigenvalue_filename)
+function  ImageCode = MSS()%ImageCode =MSS(fbase_filename, eigenvalue_filename)
 %Multiscale Spectra Signature
 
-%fbase_filename = '~/data/meshtk_workshop/mesh2.fbase';
-%eigenvalue_filename = '~/data/meshtk_workshop/mesh2.ev/_ev.ascii';
+fbase_filename = '~/data/meshtk_workshop/mesh.fbase';
+eigenvalue_filename = '~/data/meshtk_workshop/mesh.ev/_ev.ascii';
 [L D] = TriMeshTKFBaseRead(fbase_filename, eigenvalue_filename);
 
 L = L(1:100,1:100);
@@ -38,46 +38,69 @@ threshold = log(numberofeigenvalues);
 pixalheight = threshold/numberofsteps;
 ImageCode = zeros(numberofsteps,numberofsteps);
 
+% for t = 1:numberofsteps
+%     sortE = sort((E(:,t)+threshold)/pixalheight, 'descend');    
+%     tag = sum(sortE>0);
+%     if (tag == length(sortE))
+%         sortE(end+1) = -sortE(end);
+%     end
+%     
+%     for j = 1:numberofsteps
+%         while (j-0.5>sortE(tag)) 
+%             tag = tag -1;
+%         end
+%         
+%         if (j-.5-sortE(tag+1) > sortE(tag) -j +.5)
+%             ImageCode(j,t) = sortE(tag) - j +.5;
+%         else
+%             ImageCode(j,t) = j-.5 - sortE(tag+1);
+%         end
+%         
+%     end
+% end
+
+%ImageCode = ImageCode / numberofsteps;
+%imshow(1-ImageCode);
+
+m = 0;
+
 for t = 1:numberofsteps
     sortE = sort((E(:,t)+threshold)/pixalheight, 'descend');    
     tag = sum(sortE>0);
-    if (tag == length(sortE))
-        sortE(end+1) = -sortE(end);
-    end
-    
-    for j = 1:numberofsteps
-        while (j-0.5>sortE(tag)) 
-            tag = tag -1;
-        end
-        
-        if (j-.5-sortE(tag+1) > sortE(tag) -j +.5)
-            ImageCode(j,t) = sortE(tag) - j +.5;
-        else
-            ImageCode(j,t) = j-.5 - sortE(tag+1);
-        end
-        
+    if (tag ~= length(sortE) && -sortE(tag+1)>m)
+        m = -floor(sortE(tag+1));        
     end
 end
 
-ImageCode = ImageCode / numberofsteps;
-%imshow(1-ImageCode);
+ImageFMHeight = numberofsteps + m;
+ImageFM = zeros(ImageFMHeight, numberofsteps);
 
-% for t = 1:time_c
-%     for j=2:numberofeigenvalues
-%         s = E(j,t) + threshold;
-%         if (s>0) 
-%             i=floor(s/pixalheight+.5);
-%             if (i<numberofsteps) 
-%                 ImageCode(i+1,t) = ImageCode(i+1,t) + s - i*pixalheight;
-%             end
-%             if (i>0)
-%                 ImageCode(i,t) = ImageCode(i,t) + (i+1)*pixalheight -s;
-%             end
-%         end
-%     end
-% end
-% 
-% ImageCode(ImageCode>1) = 1;
-% ImageCode = 1 - ImageCode;
+threshold = ImageFMHeight * pixalheight;
+
+for t = 1:time_c
+    for j=1:numberofeigenvalues
+        s = E(j,t) + threshold;
+        if (s>0) 
+            i=floor(s/pixalheight+.5);
+            if (i<m) 
+                ImageFM(i+1,t) = ImageFM(i+1,t) + s - i*pixalheight;
+            end
+            if (i>0)                
+                ImageFM(i,t) = ImageFM(i,t) + (i+1)*pixalheight -s;
+            end
+        end
+    end
+end
+ImageFM(ImageCode>1) = 1;
+
+[row,col] = find(ImageFM>0);
+SourcePoints = [row';col'];
+SourceIndex = row + ImageFMHeight*(col-1);
+SourceValues = 1- ImageFM(SourceIndex);
+ImageSpeed = ones(ImageFMHeight, numberofsteps);
+ImageFM = msfm2d(ImageSpeed, SourcePoints, SourceValues, false, false);
+
+ImageCode = ImageFM(end - numberofsteps +1:end,:)/100;
+%imshow(-ImageCode, [])
 
 end
